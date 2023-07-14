58449868048299ee692b755892d91615690a0074
Oleh Semenenko <o.semenenko.mar@gmail.com>
2023-07-14 14:04:57 +0300

    implement multiple selection of recipes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, MouseEvent } from "react";
 import create from "zustand";
 import { type IRecipe } from "./types/global.types";
 import BeerCard from "./components/BeerCard/BeerCard";
@@ -14,6 +14,7 @@ const useRecipeStore = create((set) => ({
 export default function Home() {
   const [loadedRecipes, setLoadedRecipes] = useState<IRecipe[]>([]);
   const [loadMore, setLoadMore] = useState(false);
+  const [selectedRecipes, setSelectedRecipes] = useState<number[]>([]);
 
   const { recipes, setRecipes } = useRecipeStore() as {
     recipes: IRecipe[];
@@ -25,7 +26,6 @@ export default function Home() {
       const response = await fetch("https://api.punkapi.com/v2/beers?page=1");
       const data = await response.json();
       setRecipes(data);
-      setLoadedRecipes(data.slice(0, 15));
     } catch (error) {
       console.log(error);
     }
@@ -33,33 +33,68 @@ export default function Home() {
 
   useEffect(() => {
     fetchRecipes();
+  }, []);
 
+  useEffect(() => {
     const totalRecipes = recipes.length;
     if (totalRecipes > 15) {
-      setLoadedRecipes(recipes.slice(15, 25));
+      setLoadedRecipes(recipes.slice(0, 15));
       setLoadMore(true);
     } else {
-      setLoadedRecipes([]);
+      setLoadedRecipes(recipes);
       setLoadMore(false);
     }
   }, [recipes]);
+
+  const handleRecipe = (e: MouseEvent, id: number) => {
+    e.preventDefault();
+
+    if (e.button === 2) {
+      if (selectedRecipes.includes(id)) {
+        setSelectedRecipes((prevSelectedRecipes) =>
+          prevSelectedRecipes.filter((recipeId) => recipeId !== id)
+        );
+      } else {
+        setSelectedRecipes((prevSelectedRecipes) => {
+          console.log(prevSelectedRecipes);
+          return [...prevSelectedRecipes, id];
+        });
+      }
+    }
+  };
+
+  const handleDelete = (selectedRecipes: number[]) => {
+    const updatedRecipes = recipes.filter((recipe: IRecipe) => !selectedRecipes.includes(recipe.id))
+    setRecipes(updatedRecipes)
+    setSelectedRecipes([]);
+  };
+
   return (
-    <main className={css.container}>
-      <h1>Beer recipes</h1>
+    <div className={css.container}>
+      <div className={css.header}>
+        <h1>Beer recipes</h1>
+
+        {selectedRecipes.length > 0 && (
+          <button
+            className={css.deleteButton}
+            onClick={() => handleDelete(selectedRecipes)}
+          >
+            Delete
+          </button>
+        )}
+      </div>
 
       <ul className={css.list}>
-        {recipes.slice(0, 15).map((recipe: IRecipe) => (
-          <li key={recipe.id}>
+        {loadedRecipes.map((recipe: IRecipe) => (
+          <li
+            key={recipe.id}
+            onContextMenu={(e) => handleRecipe(e, recipe.id)}
+            className={selectedRecipes.includes(recipe.id) ? css.selected : ""}
+          >
             <BeerCard recipe={recipe} />
           </li>
         ))}
-        {loadMore &&
-          loadedRecipes.map((recipe: IRecipe) => (
-            <li key={recipe.id}>
-              <BeerCard recipe={recipe} />
-            </li>
-          ))}
       </ul>
-    </main>
+    </div>
   );
 }
